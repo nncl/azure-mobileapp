b04eaaa85e08cc3eaaeb899b06cfca983f3318b0
Cauê Almeida <nncl@live.com>
2017-05-24 21:14:25 -0300

    Initial commit

---
+++ nnclmobileappfiap_Xamarin_Forms/nnclmobileappfiap/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace nnclmobileappfiap
+{
+	public static class Constants
+	{
+		// Replace strings with your Azure Mobile App endpoint.
+		public static string ApplicationURL = @"https://nnclmobileappfiap.azurewebsites.net";
+	}
+}
+
