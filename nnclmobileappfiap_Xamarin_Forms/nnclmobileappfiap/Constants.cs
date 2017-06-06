4a7e06c6179b399ff977433fa6cd2825f80c2f90
Cauê Almeida <nncl@live.com>
2017-06-05 21:35:53 -0300

    auth to leos api

--- nnclmobileappfiap_Xamarin_Forms/nnclmobileappfiap/Constants.cs
+++ nnclmobileappfiap_Xamarin_Forms/nnclmobileappfiap/Constants.cs
@@ -4,8 +4,9 @@ namespace nnclmobileappfiap
 {
 	public static class Constants
 	{
-		// Replace strings with your Azure Mobile App endpoint.
-		public static string ApplicationURL = @"https://nnclmobileappfiap.azurewebsites.net";
-	}
+        // Replace strings with your Azure Mobile App endpoint.
+        // public static string ApplicationURL = @"https://nnclmobileappfiap.azurewebsites.net";
+        public static string ApplicationURL = @"https://xfclouddblcv.azurewebsites.net";
+    }
 }
 
---
+++ nnclmobileappfiap_Xamarin_Forms/nnclmobileappfiap/Professor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.WindowsAzure.MobileServices;
+using Newtonsoft.Json;
+
+namespace nnclmobileappfiap
+{
+    public class Professor
+    {
+        string id;
+        string rm;
+        string nome;
+        string email;
+        bool aprovado;
+        // TODO IsAprovado retornando a flag <- localDB
+
+        [JsonProperty(PropertyName = "id")]
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        [JsonProperty(PropertyName = "rm")]
+        public string Rm
+        {
+            get { return rm; }
+            set { rm = value; }
+        }
+
+        [JsonProperty(PropertyName = "email")]
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        [JsonProperty(PropertyName = "nome")]
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        [JsonProperty(PropertyName = "complete")]
+        public bool Aprovado
+        {
+            get { return aprovado; }
+            set { aprovado = value; }
+        }
+    }
+}
